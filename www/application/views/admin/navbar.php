d68416e05905f2f20da810122407ce3cb4489013
Viktor-Melnikov <vik.melnikov@gmail.com>
2013-08-27 23:46:30 +0400

    edit popup.css

--- www/application/views/admin/navbar.php
+++ www/application/views/admin/navbar.php
@@ -20,7 +20,7 @@
                     <a href="#" id="user_name"><span class="login"><i class="icon-user"></i> <?=$session->get('first_name').' '.$session->get('last_name')?></span><strong class="caret" style="margin-left: 2px;margin-top: 8px;"></strong></a>
                     <div id="popup" class="hide">
                         <div class="pull-left">
-                            <img class="img-login" style="width: 95px; height: 95px" src="<?=$session->get('photo')?>"/>
+                            <img class="img-login" style="width: 95px; height: 95px" src="<?=URL::site($session->get('photo'))?>"/>
                         </div>
                         <div class="pull-right" style="width: 170px">
                             <span style="font-weight: bolder"">  <?=$session->get('first_name').' '.$session->get('last_name')?></span><br>

73483c91b5d931797b5ccea7ff738bd1eb2aaae5
Viktor-Melnikov <vik.melnikov@gmail.com>
2013-08-27 22:58:05 +0400

    епать

--- kjj.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Developer: Roquie
- * DateTime: 27.08.13 21:18
- * Current file name: kjj.php
- * 
- * All rights reserved (c)
- */
--- йцу.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Виктор
- * Date: 27.08.13
- * Time: 22:44
- * To change this template use File | Settings | File Templates.
- */

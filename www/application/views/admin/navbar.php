815a5a3ba02f2cd9bbcc4f4168a63a2807cf7e39
Roquie <roquie0@gmail.com>
2013-08-28 20:22:13 +0400

    admin navbar mini fix, active menu "admin"

--- www/application/views/admin/navbar.php
+++ www/application/views/admin/navbar.php
@@ -3,8 +3,8 @@
         <div class="nav-collapse">
             <ul class="nav">
                 <li><a href="<?=URL::site()?>"><i class="icon-home"></i> Главная</a></li>
-                <li><a href="<?=URL::site('/admin')?>"><i class="icon-bar-chart"></i> Админка</a></li>
-                <li class="dropdown active">
+                <li class="active"><a href="<?=URL::site('/admin')?>"><i class="icon-bar-chart"></i> Админка</a></li>
+                <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-th-list"></i> Щито-то<b class="caret"></b></a>
                     <ul class="dropdown-menu">
                         <li><a href="<?=URL::site('admin/papers')?>">Просмотр</a></li>

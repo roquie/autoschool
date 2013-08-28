c62edf914d1eb5f51aba91e2a2b3b5fb8c6c36ac
Roquie <roquie0@gmail.com>
2013-08-28 21:04:51 +0400

    img

--- www/application/views/admin/google.php
+++ www/application/views/admin/google.php
@@ -2,7 +2,7 @@
     <h1><small>Загрузить файлы в Google Drive</small></h1>
     <div class="well">
         <?foreach($statements as $statement):?>
-            <ul>
+            <ul style="list-style: none">
                 <li><a href="#"><?=$statement?></a></li>
             </ul>
         <?endforeach?>

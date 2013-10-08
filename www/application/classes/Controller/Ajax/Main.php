0d03b5409c0d049f816d9004de52d9f5e1199d89
Roquie <Roquie0@gmail.com>
2013-10-08 11:58:22 +0400

    Update Main.php
    comment

--- www/application/classes/Controller/Ajax/Main.php
+++ www/application/classes/Controller/Ajax/Main.php
@@ -7,7 +7,8 @@ class Controller_Ajax_Main extends Controller_Ajax_Ajax
     {
         parent::before();
 
-        // мыло юзера сайта (проверять незя т.к. тут есть в LK_Ajax есть метод подачи доков)
+        // мыло юзера сайта (проверять незя т.к. тут есть в LK_Ajax есть метод подачи доков) 
+        // уже этого метоа нету, значит мона проверять
         //$email = Cookie::get('userEmail'); if (!is_null($email)) HTTP::redirect('lk');
 
         $this->auto_render = false;

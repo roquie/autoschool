cdf690148c3ce2706ab85ce74c943e858ebe1192
Roquie <roquie0@gmail.com>
2013-10-10 19:40:32 +0400

    чтото было сделано

---
+++ www/application/classes/Model.php
@@ -0,0 +1,115 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+class Model extends Kohana_Model
+{
+    protected static $_model_name = '';
+
+    /**
+     * Create a new model instance.
+     *
+     *     $model = Model::factory($name);
+     *
+     * @param   string  $name   model name
+     * @return  Model
+     */
+    public static function factory($name)
+    {
+        Model::$_model_name = $name;
+        // Add the model prefix
+        $class = 'Model_'.$name;
+        return new $class;
+    }
+
+    /**
+     * @return Database_Result
+     * @throws HTTP_Exception_404
+     */
+    public function all()
+    {
+        $data = ORM::factory(Model::$_model_name)->find_all();
+        if ($data->count() > 0)
+            return $data;
+        else
+            throw new HTTP_Exception_404();
+    }
+
+    /**
+     * @param array $data
+     * @param array $only
+     *
+     * @return bool
+     * @throws HTTP_Exception_404
+     */
+    public function addRec(array $data, array $only = null)
+    {
+        $result = ORM::factory(Model::$_model_name)
+                      ->values($data, $only)
+                      ->create();
+
+        if ($result->loaded())
+            return true;
+        else
+            throw new HTTP_Exception_404();
+    }
+
+    /**
+     * @param $where
+     * @param $value
+     *
+     * @return ORM
+     * @throws HTTP_Exception_404
+     */
+    public function getBy($where, $value)
+    {
+        $data = ORM::factory(Model::$_model_name)->where($where, '=', $value)->find();
+
+        if ($data->loaded())
+            return $data;
+        else
+            throw new HTTP_Exception_404();
+
+    }
+
+    /**
+     * @param       $id
+     * @param       $data
+     * @param array $only
+     *
+     * @return bool
+     * @throws HTTP_Exception_404
+     */
+    public function upd($id, $data, array $only = null)
+    {
+        $result = ORM::factory(Model::$_model_name, $id)
+                    ->values($data, $only)
+                    ->update();
+
+        if ($result->loaded())
+            return true;
+        else
+            throw new HTTP_Exception_404();
+
+    }
+
+    /**
+     * @param $id
+     *
+     * @return bool
+     * @throws HTTP_Exception_404
+     */
+    public function del($id)
+    {
+        $result = ORM::factory(Model::$_model_name, (int)$id);
+
+        if ($result->loaded()) {
+            $result->delete();
+            return true;
+        } else {
+            throw new HTTP_Exception_404();
+        }
+
+    }
+
+
+
+}
--- www/application/views/admin/mail/send.php
+++ www/application/views/admin/mail/send.php
@@ -1,22 +1,67 @@
-<?=HTML::style('css/send.css')?>
+<?=HTML::style('css/admin/sendmsg.css')?>
 <?=HTML::style('media/css/style.css')?>
-<?=HTML::script('media/js/jquery.knob.js')?>
+<?=HTML::style('css/vendor/chosen.css')?>
 <?=HTML::script('media/js/jquery.ui.widget.js')?>
-<?=HTML::script('media/js/jquery.iframe-transport.js')?>
-<?=HTML::script('media/js/jquery.fileupload.js')?>
-<?=HTML::script('media/js/jquery.fileupload-process.js')?>
-<?=HTML::script('media/js/jquery.fileupload-validate.js')?>
 <?=HTML::script('js/mail.js')?>
+<?=HTML::script('js/vendor/chosen.jquery.min.js')?>
+<?//=HTML::script('js/equalHeight.js')?>
+
 <script>
     $(function() {
-        equalHeight($('#formSend'), $('#listStd'));
+
+        //equalHeight($('#formSend'), $('#listStd'));
+
+        $('#grajdanstvo').chosen({
+            no_results_text: "Ничего не найдено"
+        });
+
+
     });
+
 </script>
 <div class="container">
     <div class="span12">
 
         <h1><small>Рассылка писем</small></h1>
         <div class="row">
+            <!--
+                короче мы все обсудили
+            -->
+            <div class="span4" style="height: 450px">
+                <div class="well" id="listStd">
+                    <div class="container-fluid">
+                        <legend>Список адресатов</legend>
+                        <!--  rel='tooltip' title="Кликните для добавления адресата"  -->
+                        <div class="check emails">
+                            Чат со слушателем
+                            <input type="checkbox" value="vik.melnikov@gmail.com" />
+                        </div>
+                        <p style="margin: 10px 0 0 0">Выберите группу</p>
+                        <select id="grajdanstvo" name="statement[nationality_id]" style="width: 219px" data-req="true">
+                            <?for($i = 0; $i <= 40; $i++):?>
+                                <option>Группа - <?=$i?></option>
+                            <?endfor?>
+                        </select>
+                        <p style="margin: 0">тут та штука с поиском</p>
+                        <div id="slimScroll" class="checkboxes" style="height: 341px; overflow: hidden">
+                            <? for($i = 0; $i <= 99; $i++):
+                                if ($i % 2 === 0): ?>
+                                    <div class="check emails">
+                                        <?=$i?> Мельников В.И.
+                                        <input type="checkbox" value="vik.melnikov@gmail.com" />
+                                    </div>
+                                    <?//else:?>
+                                    <div class="check emails">
+                                        Иванов М.В.
+                                        <input type="checkbox" value="roquie0@gmail.com" />
+                                    </div>
+                                <?endif?>
+                            <?endfor?>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
 
             <div class="span8">
 
@@ -35,25 +80,13 @@
                             ->set('id_div_editor', 'editor')
                             ->set('script_load', true)
                             ->render()?>
-                        <!---->
-                        <div id="uploadFiles">
-                            <ul></ul>
-                        </div>
-                        <!--контейнер с кнопками Отправить и Очистить-->
+
                         <div style="margin-top: 10px">
                             <input type="submit" class="btn btn-success" value="Отправить">
                     </form>
 
                             <button type="button" id="reset" class="btn">Очистить</button>
                             <div class="pull-right">
-                                <div class="btn-group dropup">
-                                    <span class="btn" id="fileUpload">
-                                        <i class="icon-upload-alt"></i>
-                                        <input id="upload" type="file" name="upl" data-url="<?=URL::site('uploadifive')?>" multiple>
-                                    </span>
-                                    <!--<button  type="button" class="btn"></i></button>-->
-                                    <a href="#upload_files" rel="tooltip" data-toggle="modal" data-url="<?=URL::site('admin/mail/getFiles')?>" title="Прикрепить файл к письму" id="attacheModal" class="btn"><i class="icon-file-alt"></i></a>
-                                </div>
                                 <div class="btn-group dropup">
                                     <a href="<?=URL::site('admin/tpl/')?>" class="btn">Шаблоны</a>
                                     <button class="btn dropdown-toggle" data-toggle="dropdown">
@@ -71,48 +104,8 @@
             </div>
 
 
-            <div class="span4">
-                <div class="well" id="listStd">
-                    <div class="container-fluid">
-                        <legend>Список адресатов</legend>
-                        <!--  rel='tooltip' title="Кликните для добавления адресата"  -->
-                        <div id="slimScroll" class="checkboxes" style="height: 390px; overflow: hidden">
-                            <? for($i = 0; $i <= 99; $i++):
-                                if ($i % 2 === 0): ?>
-                            <div class="check emails">
-                                <?=$i?> Мельников В.И.
-                                <input type="checkbox" value="vik.melnikov@gmail.com" />
-                            </div>
-                                    <?//else:?>
-                            <div class="check emails">
-                                 Иванов М.В.
-                                <input type="checkbox" value="roquie0@gmail.com" />
-                            </div>
-                            <?endif?>
-                            <?endfor?>
-                        </div>
-                        <div style="margin-top: 10px">
-                            <button rel='tooltip' title="Добавить" class="btn"><i class="icon-plus"></i></button>
-                            <button rel='tooltip' title="Редактировать" class="btn"><i class="icon-edit"></i></button>
-                            <button rel='tooltip' title="Удалить" class="btn"><i class="icon-trash"></i></button>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
 
     </div>
 </div>
 
-<!-- Modal -->
-<div id="upload_files" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-        <h3 id="myModalLabel">Загрузить файлы</h3>
-    </div>
-    <div class="modal-body"></div>
-    <div class="modal-footer">
-        <button data-dismiss="modal" aria-hidden="true" class="pull-left btn btn-primary">Готово</button>
-    </div>
-</div>
-<!-- end Modal -->

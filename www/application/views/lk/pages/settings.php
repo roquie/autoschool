fc2879351cb4a740e8726af8c3fd85b6d27b4fc3
Viktor-Melnikov <vik.melnikov@gmail.com>
2013-11-09 15:00:13 +0300

    Раздел настройки в ЛК исправлен

--- www/application/views/lk/pages/settings.php
+++ www/application/views/lk/pages/settings.php
@@ -1,4 +1,9 @@
 <?=HTML::style('css/lk/lk_sttgs.css')?>
+<script>
+    $(function() {
+        $('._lk_form').ajaxForm();
+    });
+</script>
 <div class="row settingsContent">
     <div class="span4">
         <h2>Изменить пароль</h2>
@@ -19,7 +24,7 @@
     <div class="span4">
         <h2>Изменить email</h2>
 
-        <form id="check_pass" action="<?=URL::site('lk/ajax/check_pass')?>" class="_lk_form" data-callback="check_pass" method="POST" >
+        <form id="toggle_pass_email" action="<?=URL::site('lk/ajax/check_pass')?>" class="_lk_form" data-callback="check_pass" method="POST" >
             <p>Подтвердите свои действия:</p>
             <div class="input-prepend">
                 <span class="add-on"><i class="icon-key"></i></span>
@@ -29,7 +34,7 @@
             <input type="submit" value="Далее" class="btn btn-success pull-right"/>
         </form>
 
-        <form id="change_email" action="<?=URL::site('lk/ajax/change_email')?>" method="POST" class="_lk_form" style="padding-bottom: 50px; display: none" novalidate>
+        <form id="toggle_pass_email" action="<?=URL::site('lk/ajax/change_email')?>" data-callback="change_email" method="POST" class="_lk_form" style="padding-top: 40px; display: none" novalidate>
             <div class="input-prepend">
                 <span class="add-on"><b style="font-weight: 600">@</b></span>
                 <input id="new_email" name="new_email" placeholder="новый email" type="email">

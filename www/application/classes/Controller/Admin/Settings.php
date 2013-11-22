1a267de86ae7d48891815f8740db5c87224ed4c5
roquie <roquie0@gmail.com>
2013-11-22 17:50:45 +0400

    работают настройки без autocrud. пичалька

--- www/application/classes/Controller/Admin/Settings.php
+++ www/application/classes/Controller/Admin/Settings.php
@@ -38,42 +38,7 @@ class Controller_Admin_Settings extends Controller_Ajax_Admin
         else
             $this->ajax_msg('Ошибка', 'error');
     }*/
-    /**
-     *
-     * @type ajax
-     * */
-    public function action_create()
-    {
-        $checkEmail = Model::factory('Administrators')->getBy('email', Arr::get($_POST, 'email'));
 
-        if (!$checkEmail) {
-            $result = Model::factory('Administrators')
-                   ->addRec(array(
-                       'email' => Arr::get($_POST, 'email'),
-                       'datetime' => date('Y-m-d H:i:s'),
-                   ));
-            if ($result)
-                $this->ajax_msg('Пользователь добавлен');
-            else
-                $this->ajax_msg('ошибка бд', 'error');
-
-        } else
-            $this->ajax_msg('Такой админ уже есть', 'error');
-
-    }
-
-
-    /**
-     *
-     */
-    public function action_delete()
-    {
-
-        $id = Arr::get($_POST, 'id');
-        Model::factory('Administrators')->del($id);
-
-        $this->ajax_msg($id);
-    }
 
 
 }

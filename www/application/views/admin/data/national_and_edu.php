a61bbff60c53bd7eebba8a2aaadb58cb2d7f47d6
roquie <roquie0@gmail.com>
2014-01-16 23:32:48 +0400

    страница Гражданство и образование

--- www/application/views/admin/data/national_and_edu.php
+++ www/application/views/admin/data/national_and_edu.php
@@ -1,21 +1,76 @@
 <div class="container">
     <div class="row">
-        <div class="span4 pull-left">
-            <h1><small>Заголовок</small></h1>
-        </div>
-        <div class="span8" style="margin-top: 20px">
-            <!--<div class="pull-right">
-                <div class="btn-group">
-                    <a href="<?/*=URL::site('/print/pdf')*/?>" target="_blank" rel="tooltip" title="Распечатать список слушателей" class="btn"><i class="icon-print"></i></a>
-                    <a href="<?/*=URL::site('/download/print/name')*/?>" rel="tooltip" title="Загрузить документ со списоком слушателей" class="btn btn-success"><i class="icon-download"></i></a>
-                    <a href="#" rel="tooltip" title="Открыть список слушателей" class="btn btn-info"><i class="icon-eye-open"></i></a>
-                </div>
-                <a href="#" class="btn btn-success" data-placement="left" rel="tooltip" title="Распределение слушателей подавших заявку по группам">Кнопка</a>
+        <div class="span12 pull-left">
+            <h1><small>Гражданство и образование</small></h1>
+            <!--<div class="span8 offset2">
+                <p style="text-align: center">Эта страница необходима для добавления одноименных полей в БД, для последующего их вывода на странице заполнения заявления.</p>
             </div>-->
         </div>
     </div>
 
     <div class="row">
+        <div class="span6">
+            <div class="well">
+                <h5 class="header_block">Гражданство</h5>
+                <form  action="#" method="post" accept-charset="utf-8" novalidate>
+                    <label for="national_add">Добавить</label>
+                    <input id="national_add" type="text" name="add"/>
+                    <input type="submit" style="margin-top: -10px" class="btn" value="OK"/>
+                </form>
+                <table class="table table-bordered">
+                    <thead>
+                    <tr>
+                        <th style="width: 20px">#</th>
+                        <th style="width: 120px">Наименование</th>
+                        <th style="width: 50px">Редакт.</th>
+                        <th style="width: 50px">Удал.</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?for($i=1; $i<=5; ++$i):?>
+                        <tr>
+                            <td><?=$i?></td>
+                            <td>РФ</td>
+                            <td><a style="display: table; margin: 0 auto" href="#" class="badge"><i class="icon-pencil"></i></a></td>
+                            <td><a style="display: table; margin: 0 auto"  href="#" class="badge badge-important"><i class="icon-remove"></i></a></td>
+                        </tr>
+                    <?endfor?>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="span6">
+            <div class="well">
+                <h5 class="header_block">Образование</h5>
+                <form  action="#" method="post" accept-charset="utf-8" novalidate>
+                    <label for="national_add">Добавить</label>
+                    <input id="national_add" type="text" name="add"/>
+                    <input type="submit" style="margin-top: -10px" class="btn" value="OK"/>
+                </form>
+                <table class="table table-bordered">
+                    <thead>
+                    <tr>
+                        <th style="width: 20px">#</th>
+                        <th style="width: 120px">Наименование</th>
+                        <th style="width: 50px">Редакт.</th>
+                        <th style="width: 50px">Удал.</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?for($i=1; $i<=5; ++$i):?>
+                        <tr>
+                            <td><?=$i?></td>
+                            <td>Высшее</td>
+                            <td><a style="display: table; margin: 0 auto" href="#" class="badge"><i class="icon-pencil"></i></a></td>
+                            <td><a style="display: table; margin: 0 auto"  href="#" class="badge badge-important"><i class="icon-remove"></i></a></td>
+                        </tr>
+                    <?endfor?>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
         <!--<input type="hidden" name="csrf" value="<?/*=Security::token()*/?>"/>-->
 
 

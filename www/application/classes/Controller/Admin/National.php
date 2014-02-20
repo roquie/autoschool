9b798660d435438eebbea2dd10e06d271f351d16
roquie <roquie0@gmail.com>
2014-02-20 18:03:55 +0400

    убран тупизм

--- www/application/classes/Controller/Admin/National.php
+++ www/application/classes/Controller/Admin/National.php
@@ -4,60 +4,55 @@
 class Controller_Admin_National extends Controller_Ajax_Admin
 {
 
+    public function before()
+    {
+        parent::before();
+
+        if (!Security::is_token($this->request->post('csrf')))
+        {
+            throw new HTTP_Exception_404();
+        }
+    }
+
     public function action_create()
     {
         $post = $this->request->post('data');
-        if (Security::is_token($post['csrf']))
+        try
         {
-            try
-            {
-                $id = ORM::factory('Nationality')
-                         ->values($post)
-                         ->create()
-                         ->pk();
+            $id = ORM::factory('Nationality')
+                     ->values($post)
+                     ->create()
+                     ->pk();
 
-                $this->ajax_data(
-                    array('id' => $id),
-                    'Запись добавлена'
-                );
-            }
-            catch (ORM_Validation_Exception  $e)
-            {
-                $errors = $e->errors('validation');
-                $this->ajax_msg(array_shift($errors), 'error');
-            }
+            $this->ajax_data(
+                array('id' => $id),
+                'Запись добавлена'
+            );
         }
-        else
+        catch (ORM_Validation_Exception  $e)
         {
-            throw new HTTP_Exception_404();
+            $errors = $e->errors('validation');
+            $this->ajax_msg(array_shift($errors), 'error');
         }
     }
 
     public function action_read()
     {
         $post = $this->request->post();
-        if (Security::is_token($post['csrf']))
+        try
         {
-            try
-            {
-                $result = ORM::factory('Nationality')
-                             ->find_all();
-                $data = array();
-                foreach ($result as $value)
-                    $data[] = $value->as_array();
+            $result = ORM::factory('Nationality')
+                         ->find_all();
+            $data = array();
+            foreach ($result as $value)
+                $data[] = $value->as_array();
 
-                $this->ajax_data($data);
-            }
-            catch (Exception  $e)
-            {
-                $this->ajax_msg($e->getMessage(), 'error');
-            }
+            $this->ajax_data($data);
         }
-        else
+        catch (Exception  $e)
         {
-            throw new HTTP_Exception_404();
+            $this->ajax_msg($e->getMessage(), 'error');
         }
-
     }
 
     public function action_delete()
@@ -65,25 +60,18 @@ class Controller_Admin_National extends Controller_Ajax_Admin
         $id = $this->request->param('id');
         $post = $this->request->post('data');
 
-        if (Security::is_token($post['csrf']))
+        try
         {
-            try
-            {
-                ORM::factory('Nationality', $id)
-                   ->delete();
+            ORM::factory('Nationality', $id)
+               ->delete();
 
-                $this->ajax_data(array(
-                    'id' => $id
-                ), 'Запись удалена');
-            }
-            catch (Exception  $e)
-            {
-                $this->ajax_msg($e->getMessage(), 'error');
-            }
+            $this->ajax_data(array(
+                'id' => $id
+            ), 'Запись удалена');
         }
-        else
+        catch (Exception  $e)
         {
-            throw new HTTP_Exception_404();
+            $this->ajax_msg($e->getMessage(), 'error');
         }
 
     }

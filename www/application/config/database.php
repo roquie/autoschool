ea149da2d108ed427fbd8119e5cf1b512e33ffaf
roquie <roquie0@gmail.com>
2014-02-21 15:50:08 +0400

    configs

--- www/application/config/database.php
+++ www/application/config/database.php
@@ -6,7 +6,7 @@ return array(
         'type'       => 'MySQL',
         'connection' => array(
             'hostname'   => 'localhost',
-            'username'   => 'melnik5g_ampt',    //zxc
+            'username'   => 'melnik5g_ampt',    //zxcfhggf
             'password'   => 'qweasdzxc',
             'persistent' => FALSE,
             'database'   => 'melnik5g_ampt',
